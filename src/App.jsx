25696c474c82d4c4b0b6ee086182b7fcda8d3948
Marketlube <marketlubesoftwares@MarketlubesMini.Dlink>
2024-11-19 19:11:14 +0530

    first section

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Robo } from "./SectionOne/robo";
+
+function App() {
+  return (
+    <div>
+      <Robo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/SectionOne/Robo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Robo = () => {
+  return (
+    <div className="robot-container">
+      <div className="robot-image" />
+      <div className="overlay-text">The New Age Marketing Agency</div>
+    </div>
+  );
+};
