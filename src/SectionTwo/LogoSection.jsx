0dad3d7fc3d1b36103881b3a9b294025d7f4e988
market lube <marketlubes@gmail.com>
2024-12-26 14:26:44 +0530

    upto last change

--- src/SectionTwo/LogoSection.jsx
+++ src/SectionTwo/LogoSection.jsx
@@ -2,44 +2,47 @@ export const LogoSection = () => {
   const logos = Array.from({ length: 15 }, (_, i) => `Logo ${i + 1}`);
 
   return (
-    <div className="logos">
-      <div className="logo-slide">
-        {/* Render two sets of logos for seamless looping */}
-        {logos.map((logo, index) => (
-          <div key={`logo-${index}`}>{logo}</div>
-        ))}
-        {logos.map((logo, index) => (
-          <div key={`logo-duplicate-${index}`}>{logo}</div>
-        ))}
-        {logos.map((logo, index) => (
-          <div key={`logo-duplicate-${index}`}>{logo}</div>
-        ))}
-        {logos.map((logo, index) => (
-          <div key={`logo-duplicate-${index}`}>{logo}</div>
-        ))}
-        {logos.map((logo, index) => (
-          <div key={`logo-duplicate-${index}`}>{logo}</div>
-        ))}
+    <>
+      <div className="logos">
+        <div className="logo-slide">
+          {/* Render two sets of logos for seamless looping */}
+          {logos.map((logo, index) => (
+            <div key={`logo-${index}`}>{logo}</div>
+          ))}
+          {logos.map((logo, index) => (
+            <div key={`logo-duplicate-${index}`}>{logo}</div>
+          ))}
+          {logos.map((logo, index) => (
+            <div key={`logo-duplicate-${index}`}>{logo}</div>
+          ))}
+          {logos.map((logo, index) => (
+            <div key={`logo-duplicate-${index}`}>{logo}</div>
+          ))}
+          {logos.map((logo, index) => (
+            <div key={`logo-duplicate-${index}`}>{logo}</div>
+          ))}
+        </div>
       </div>
-
-      <div className="logo-slide-two">
-        {/* Render two sets of logos for seamless looping */}
-        {logos.map((logo, index) => (
-          <div key={`logo-${index}`}>{logo}</div>
-        ))}
-        {logos.map((logo, index) => (
-          <div key={`logo-duplicate-${index}`}>{logo}</div>
-        ))}
-        {logos.map((logo, index) => (
-          <div key={`logo-duplicate-${index}`}>{logo}</div>
-        ))}
-        {logos.map((logo, index) => (
-          <div key={`logo-duplicate-${index}`}>{logo}</div>
-        ))}
-        {logos.map((logo, index) => (
-          <div key={`logo-duplicate-${index}`}>{logo}</div>
-        ))}
+      <div className="logos2">
+        <div className="logo-slide">
+          {/* Render two sets of logos for seamless looping */}
+          {logos.map((logo, index) => (
+            <div key={`logo-${index}`}>{logo}</div>
+          ))}
+          {logos.map((logo, index) => (
+            <div key={`logo-duplicate-${index}`}>{logo}</div>
+          ))}
+          {logos.map((logo, index) => (
+            <div key={`logo-duplicate-${index}`}>{logo}</div>
+          ))}
+          {logos.map((logo, index) => (
+            <div key={`logo-duplicate-${index}`}>{logo}</div>
+          ))}
+          {logos.map((logo, index) => (
+            <div key={`logo-duplicate-${index}`}>{logo}</div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
