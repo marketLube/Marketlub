8e9109f23cae93a87513dd5605a933f810dcb4f1
marketLube <marketlubes@gmail.com>
2024-12-30 15:43:53 +0530

    sl

--- src/Nav/Navbar.jsx
+++ src/Nav/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
-import logo from "../assets/logo.png";
+import logo from "../assets/LOGO.png";
 
 const Navbar = () => {
   const [active, setActive] = useState("#home");
